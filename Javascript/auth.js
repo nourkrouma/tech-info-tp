f19f4cb90ac16eb17b4a36688d17bcf866bf4632
nour <houdakrouma@gmail.com>
2021-11-13 20:11:02 +0100

    node added webpack works

---
+++ Javascript/auth.js
@@ -0,0 +1,27 @@
+// // Authentication 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBzZXtyZOsYlVw6rv9zf09Mtdcsj8YC4vs",
+//   authDomain: "techinfo-10473.firebaseapp.com",
+//   projectId: "techinfo-10473",
+//   storageBucket: "techinfo-10473.appspot.com",
+//   messagingSenderId: "694478408923",
+//   appId: "1:694478408923:web:92acd067cbfb431efa10fe",
+//   measurementId: "G-4K76QZ2JWR"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth=getAuth(firebaseConfig);
+// const df=getFirestore(firebaseConfig);
+console.log('hello world') 
---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./Javascript/auth.js":
+/*!****************************!*\
+  !*** ./Javascript/auth.js ***!
+  \****************************/
+/***/ (() => {
+
+eval("// // Authentication \n// // Import the functions you need from the SDKs you need\n// import { initializeApp } from \"firebase/app\";\n// import { getAuth } from \"firebase/auth\";\n// import { getFirestore } from \"firebase/firestore\";\n// import { getAnalytics } from \"firebase/analytics\";\n// // TODO: Add SDKs for Firebase products that you want to use\n// // https://firebase.google.com/docs/web/setup#available-libraries\n\n// // Your web app's Firebase configuration\n// // For Firebase JS SDK v7.20.0 and later, measurementId is optional\n// const firebaseConfig = {\n//   apiKey: \"AIzaSyBzZXtyZOsYlVw6rv9zf09Mtdcsj8YC4vs\",\n//   authDomain: \"techinfo-10473.firebaseapp.com\",\n//   projectId: \"techinfo-10473\",\n//   storageBucket: \"techinfo-10473.appspot.com\",\n//   messagingSenderId: \"694478408923\",\n//   appId: \"1:694478408923:web:92acd067cbfb431efa10fe\",\n//   measurementId: \"G-4K76QZ2JWR\"\n// };\n\n// // Initialize Firebase\n// const app = initializeApp(firebaseConfig);\n// const analytics = getAnalytics(app);\n// const auth=getAuth(firebaseConfig);\n// const df=getFirestore(firebaseConfig);\nconsole.log('hello world') \n\n//# sourceURL=webpack://tech-info-tp/./Javascript/auth.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./Javascript/auth.js"]();
+/******/ 	
+/******/ })()
+;

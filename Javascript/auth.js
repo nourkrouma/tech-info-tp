26e3aed45ad3b9a02d2296da64a1dcc1918b97c7
nour <houdakrouma@gmail.com>
2021-11-13 20:56:02 +0100

    Firestore data fetch works

--- Javascript/auth.js
+++ Javascript/auth.js
@@ -1,27 +1,43 @@
-// // Authentication 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Authentication 
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app'
+import {
+    getFirestore,collection,getDocs
+} from 'firebase/firestore'
+// import {getAuth} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBzZXtyZOsYlVw6rv9zf09Mtdcsj8YC4vs",
-//   authDomain: "techinfo-10473.firebaseapp.com",
-//   projectId: "techinfo-10473",
-//   storageBucket: "techinfo-10473.appspot.com",
-//   messagingSenderId: "694478408923",
-//   appId: "1:694478408923:web:92acd067cbfb431efa10fe",
-//   measurementId: "G-4K76QZ2JWR"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const auth=getAuth(firebaseConfig);
-// const df=getFirestore(firebaseConfig);
-console.log('hello world') 
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBzZXtyZOsYlVw6rv9zf09Mtdcsj8YC4vs",
+    authDomain: "techinfo-10473.firebaseapp.com",
+    projectId: "techinfo-10473",
+    storageBucket: "techinfo-10473.appspot.com",
+    messagingSenderId: "694478408923",
+    appId: "1:694478408923:web:92acd067cbfb431efa10fe",
+    measurementId: "G-4K76QZ2JWR"
+  };
+// !initialization 
+ initializeApp(firebaseConfig);
+//  !
+ const database=getFirestore();
+
+// collection reference
+const collectionRefrence=collection(database,'events')
+// get the collection data
+getDocs(collectionRefrence).then((snapshot)=>{
+    console.log(snapshot.docs)})
+//     let events=[];
+//     snapshot.docs.forEach((doc) => {
+//         events.push({...doc.data(),id:doc.id})
+        
+//     });
+//     console.log(events)
+
+// }).catch(err=>{
+//     console.log(err.message)
+// })
